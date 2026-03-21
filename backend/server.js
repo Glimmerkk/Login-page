99611d87b3d980e1e78f176cfe010256792b0782
YourGitHubUsername <kiptoo445120@gmail.com>
2026-03-22 01:00:39 +0300

    fixed port for render

--- backend/server.js
+++ backend/server.js
@@ -136,4 +136,6 @@ app.get("/test", async (req, res) => {
 });
 
 // START SERVER
-app.listen(3000, () => console.log("Server running on 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Server running on " + PORT));
